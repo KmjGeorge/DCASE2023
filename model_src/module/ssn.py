1b45e5abf63afd4ceaa751cec9f106de931653f0
LAPTOP-2Q9GHDE6\KomeijiGeorge <804235820@qq.com>
2023-04-12 21:38:32 +0800

    no message

--- model_src/module/ssn.py
+++ model_src/module/ssn.py
@@ -4,23 +4,16 @@ import torch.nn as nn
 # 子谱归一化
 class SubSpecNormalization(nn.Module):
     """
-    gamma : 缩放参数
-    beta : 偏移参数
-    s: 子谱组数
-    eps : 分母+eps防止为0
+    C: 输入通道数
+    S: 子谱组数
     """
-
-    def __init__(self, gamma, beta, S, eps=1e-5):
+    def __init__(self, C, S):
         super(SubSpecNormalization, self).__init__()
         self.S = S
-        self.gamma = gamma
-        self.beta = beta
-        self.eps = eps
+        self.bn = nn.BatchNorm2d(C * S)
 
     def forward(self, x):
         N, C, F, T = x.size()
         x = x.view(N, C * self.S, F // self.S, T)
-        mean = x.mean([0, 2, 3]).view(1, C * self.S, 1, 1)
-        var = x.var([0, 2, 3]).view([1, C * self.S, 1, 1])
-        x = self.gamma * (x - mean) / (var + self.eps).sqrt() + self.beta
+        x = self.bn(x)
         return x.view(N, C, F, T)
